d3ac81b187f18c500967f7b2e879e00842d77126
syudanardi <syudanardi@student.unimelb.edu.au>
2019-04-01 12:28:42 +1100

    user and users page

--- controllers/controller.js
+++ controllers/controller.js
@@ -5,3 +5,14 @@ module.exports.fetchMainPage =
     function(req,res){ 
         res.send("kyaaa")
     };
+
+module.exports.fetchUsers =
+    function(req,res){
+        res.send(users)
+    };
+
+module.exports.fetchUser =
+    function(req,res){
+        const user = users[req.params.id];
+        res.send(user);
+    }
--- routes/routes.js
+++ routes/routes.js
@@ -2,4 +2,6 @@ const express = require('express');
 const router = express.Router();
 const controllers = require("../controllers/controller");
 router.get('/', controllers.fetchMainPage);
+router.get('/users', controllers.fetchUsers);
+router.get('/users/:id', controllers.fetchUser);
 module.exports= router; 
